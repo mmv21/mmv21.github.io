5eddea72f06a39160484c625bca6b15ff135c402
mmv21 <120708929+mmv21@users.noreply.github.com>
2022-12-16 11:39:56 +0300

    Delete 1css_noMedia directory

--- 1css_noMedia/script.js
+++
@@ -1,7 +0,0 @@
-let startTime = performance.now()
-window.addEventListener('DOMContentLoaded', () => {
-    console.log(`DOM: ${  performance.now() - startTime}`)
-})
-window.addEventListener('load', () => {
-    console.log(`Page : ${performance.now() - startTime}`)
-})
